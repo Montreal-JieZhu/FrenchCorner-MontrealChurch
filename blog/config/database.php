b5b993f2851300c42b760402e224be40213b3987
jie zhu <zhujie8088@gmail.com>
2018-07-23 08:23:23 -0400

    update database configuration

--- blog/config/database.php
+++ blog/config/database.php
@@ -23,7 +23,7 @@ return [
       | you may use many connections at once using the Database library.
       |
      */
-    'default' => env('DB_CONNECTION2', 'mysql'),
+    'default' => env('DB_CONNECTION2', 'DB_CONNECTION1'),
     /*
       |--------------------------------------------------------------------------
       | Database Connections
